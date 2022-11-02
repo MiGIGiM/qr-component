987bbc4ff9ff18d8c43f3c2a5810d93c37f87032
Miguel Rodriguez <fm.rodriguezg@hotmail.com>
2022-11-01 22:33:50 -0600

    fix: semantic HTML markup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 
 const App = () => (
-  <section>
+  <main>
     <div className="card">
       <img src="https://i.ibb.co/qJH5rKM/image-qr-code.png" alt="image-qr-code" className="card-image"/>
       <h1 className="card-title">Improve your front-end skills by building projects</h1>
       <p className="card-body">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
     </div>
-  </section>
+  </main>
 )
 
 export default App
