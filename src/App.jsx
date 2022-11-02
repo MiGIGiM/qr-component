2771da61c58820496aecac13cef206e604341c5c
Miguel Rodriguez <fm.rodriguezg@hotmail.com>
2022-11-01 22:18:41 -0600

    fix: image not showing properly

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 const App = () => (
   <section>
     <div className="card">
-      <img src="src/assets/image-qr-code.png" alt="qr" className="card-image" />
+      <img src="https://i.ibb.co/qJH5rKM/image-qr-code.png" alt="image-qr-code" className="card-image"/>
       <h1 className="card-title">Improve your front-end skills by building projects</h1>
       <p className="card-body">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
     </div>
