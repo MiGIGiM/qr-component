8ee9832eb35d061eab7fcc512b819a249a19440e
Miguel Rodriguez <fm.rodriguezg@hotmail.com>
2022-11-01 22:14:52 -0600

    feat: qr-component challange solution

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+const App = () => (
+  <section>
+    <div className="card">
+      <img src="src/assets/image-qr-code.png" alt="qr" className="card-image" />
+      <h1 className="card-title">Improve your front-end skills by building projects</h1>
+      <p className="card-body">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+    </div>
+  </section>
+)
+
+export default App
